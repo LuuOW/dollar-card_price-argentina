502261b98da593b8dbfdd3e31e84f92fc1c5e6d0
Lucas A. Kempe <36010086+LuuOW@users.noreply.github.com>
2020-04-22 20:40:56 -0300

    Fixed Error exit program
    This error cause the program to exit when a decimal number been inserted
    The input parameter was an integer instead a float (used for decimal numbers)

--- dollar-card.py
+++ dollar-card.py
@@ -1,6 +1,6 @@
 import sys
-x = int(input("Ingrese el precio del dolar hoy: "))
-y = int(input("Ahora ingrese el valor del producto que desea: "))
+x = float(input("Ingrese el precio del dolar hoy: "))
+y = float(input("Ahora ingrese el valor en dolares del producto que desea: "))
 print("En total, con todos los impuestos pagaras: ")
 print("$",(x*3/10+x)*21/100+(x*3/10+x)*y , "Pesos Argentinos")
 input("Presione enter para salir: ")
