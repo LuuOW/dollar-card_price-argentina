4fcea4eae4fe6f13b2e806d7380fc489c3039647
Lucas A. Kempe <36010086+LuuOW@users.noreply.github.com>
2020-06-19 21:00:00 -0300

    Update dollar-card.py
    New features included¡

--- dollar-card.py
+++ dollar-card.py
@@ -1,7 +1,30 @@
 import sys
+
+print("Bienvenido")
+
 x = float(input("Ingrese el precio del dolar hoy: "))
-y = float(input("Ahora ingrese el valor en dolares del producto que desea: "))
-print("En total, con todos los impuestos pagaras: ")
-print("$",(x*3/10+x)*21/100+(x*3/10+x)*y , "Pesos Argentinos")
-input("Presione enter para salir: ")
+m = float(input("Desea comprar/importar algo fisico(1*) , un servicio digital(2*) , divisa(3*) ?: "))
+if m == 1:
+	h = input("El producto que desea comprar supera el precio de 50 dolares? si/no: ")
+	y = float(input("Ingrese el valor total en >dolares< del producto que desea: "))
+	if h == "si":
+		print("En total, con todos los impuestos pagaras: ")
+		print("$",((x*3/10+x)*21/100+(x*3/10+x)*y)*(5/10)+(x*3/10+x)*21/100+(x*3/10+x)*y , "Pesos Argentinos")
+
+	if h == "no":
+		print("En total, con todos los impuestos pagaras: ")
+		print("$",(x*3/10+x)*21/100+(x*3/10+x)*y , "Pesos Argentinos")
+	pass 
+if m == 2:
+	y = float(input("Ingrese el valor en >dolares< del producto que desea: "))
+	print("En total, con todos los impuestos pagaras: ")
+	print("$",(x*3/10+x)*21/100+(x*3/10+x)*y , "Pesos Argentinos")
+	pass
+if m == 3:
+	y = float(input("Ingrese el monto de dolares que desea comprar: "))
+	print("En total, con todos los impuestos pagaras: ")
+	print("$",(x*3/10+x)*y , "Pesos Argentinos")
+	pass
+
+z = input("Desea volver a calcular? Si/No: ")
 sys.exit()
