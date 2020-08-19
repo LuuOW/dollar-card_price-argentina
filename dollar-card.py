a20838ea697b1f19156ed4ab8c9dfae8003e1387
Lucas A. Kempe <36010086+LuuOW@users.noreply.github.com>
2020-08-19 14:05:26 -0300

    Bugs fixed¡
    in this version i fix some bugs and now the program is better xd

--- dollar-card.py
+++ dollar-card.py
@@ -6,44 +6,39 @@ print("Bienvenido")
 x = float(input("Ingrese el precio del dolar hoy: "))
 
 def calculate():
-	
+
 	m = float(input("Desea comprar/importar algo fisico(1*) , un servicio digital(2*) , divisa(3*) ?: "))
+
 	if m == 1:
-		h = input("El producto que desea comprar supera el precio de 50 dolares? Si(1*)/No(2*): ")
+
 		y = float(input("Ingrese el valor total en >dolares< del producto que desea: "))
-		if h == "1":
+
+		if y > "50":
 			print("En total, con todos los impuestos pagaras: ")
 			print("$",((x*3/10+x)*21/100+(x*3/10+x)*y)*(5/10)+(x*3/10+x)*21/100+(x*3/10+x)*y , "Pesos Argentinos")
-		if h == "2":
+
+		if y <= "50":
 			print("En total, con todos los impuestos pagaras: ")
-			print("$",(x*3/10+x)*21/100+(x*3/10+x)*y , "Pesos Argentinos")
-			pass 
+			print("$",(x*3/10+x)*21/100+(x*3/10+x)*y , "Pesos Argentinos") 
 
 	if m == 2:
 		y = float(input("Ingrese el valor en >dolares< del producto que desea: "))
 		print("En total, con todos los impuestos pagaras: ")
 		print("$",(x*3/10+x)*21/100+(x*3/10+x)*y , "Pesos Argentinos")
-		pass
 
 	if m == 3:
 		y = float(input("Ingrese el monto de dolares que desea comprar: "))
 		print("En total, con todos los impuestos pagaras: ")
 		print("$",(x*3/10+x)*y , "Pesos Argentinos")
-		pass
 
-#start the program 
 def run():
+
 	calculate()
-	z = input("Desea volver a calcular? Si(1*)/No(2*): ")
 
-	while z != 2:
+	z = input("Desea calcular otra vez? Si(1*), No(2*): ")
 
-
-		run()
-
-
-
-		pass
-	
+	if z != "2":
+		calculate()
 
 run()
+	
